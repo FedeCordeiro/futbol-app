16d34d0afebfa1a71115caef01fa095c640c0bc6
Fede Cordeiro <federico.cordeiro95@gmail.com>
2024-07-26 15:46:42 -0300

    add images and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerListComponent } from './player-list/player-list.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerListComponent
+    PlayerListComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
